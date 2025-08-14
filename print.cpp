a79f33434b9cfd0c29be1c8d1ddf5b40bd4732b8
Juan Alegría Vásquez <102867076+Vindemiatrixx@users.noreply.github.com>
2025-08-13 20:25:33 -0400

    Print.cpp actualizado

--- print.cpp
+++ print.cpp
@@ -15,6 +15,19 @@ struct Dato{
 
 void Mostrar_Datos_Alumno(Dato* datos, int tamanio){
 
+    /*
+    /    Descripción: 
+    /            Imprimir por consola los datos cargados en el parametro 'datos', mostrando los primeros 5 o menos en caso de que 'tamanio' sea menor a 5.
+    /
+    /    Parametros:
+    /            Dato* datos: Puntero/arreglo que contiene los datos a querer mostrar.
+    /            int tamanio: Tamaño del arreglo.
+    /    
+    /    Retorno:
+    /            Función tipo void, no retorna nada.
+    /    
+    */
+    
     std::cout << std::left 
                     << std::setw(8) << "ID" 
                     << std::setw(50) << "NOMBRE" 
